4d511deddecea651975dac70b6c43cd8d6aa37a5
terbaik97 <aziimmashoor1997@gmail.com>
2020-05-10 21:32:25 +0800

    Chapter_2 Question 1

---
+++ Chapter_2/Question_1.js
@@ -0,0 +1,21 @@
+//define n as a how many looping we want
+let n = 8;
+
+//define char as a symbol that want to print
+let char="#";
+
+//for loop to print n=8 times and it will print 8 rows
+for(var counter=0;counter<n;counter++)
+{
+//print a char which is #
+//note that for a second loop and so on char not refer on let char it till refer on char below that we make
+//increment
+ console.log( char )
+
+//char="#"+char process like increment 
+ char="#"+char;
+
+//before it going to loop char become="##" and the process will go on until 8 times
+  
+}
+desadasdasdasdasdasdsa
